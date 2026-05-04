bf6c8728e9b77c3038d057d032975324927beae2
gboone11 <gavin.boone@ejgallo.com>
2026-05-04 16:03:15 -0700

    encodeFetchRequest implemented

--- src/main/java/com/simplekafka/broker/Protocol.java
+++ src/main/java/com/simplekafka/broker/Protocol.java
@@ -22,9 +22,12 @@ public class Protocol {
     public static final byte TOPIC_NOTIF = 0x23;
 
     public static ByteBuffer encodeProduceRequest(String topic, int partition, byte[] message) {
-        // Request Type (1) + topic string len (2) + Topic string (n) + Partition (4) +
-        // message len (4) + message (n)
-        // 11 static bytes + topic len + message len
+        /**
+        * Request Type (1) + 
+        * topic string len (2) + Topic string (n) + 
+        * Partition (4) +
+        * message len (4) + message (n)
+        */
         ByteBuffer buf = ByteBuffer.allocate(11 + topic.length() + message.length);
         buf
             .put(PRODUCE)
@@ -33,6 +36,25 @@ public class Protocol {
             .putInt(partition)
             .putInt(message.length)
             .put(message);
+        return buf;
+    }
+
+    public static ByteBuffer encodeFetchRequest(String topic, int partition, long offset, int maxBytes) {
+        /**
+        * Request Type (1) + 
+        * topic string len (2) + Topic string (n) + 
+        * Partition (4) +
+        * offset (8) + maxBytes (4)
+        */
+        ByteBuffer buf = ByteBuffer.allocate(19 + topic.length());
+        buf
+            .put(FETCH)
+            .putShort((short) topic.length())
+            .put(topic.getBytes())
+            .putInt(partition)
+            .putLong(offset)
+            .putInt(maxBytes)
+            .flip();
         return buf;
     }
 }
